0ec6f4d9bdf919b5deac47272cbf6431f79fe1b0
darrylemiles <darryle.jafdigitalgroupinc@gmail.com>
2026-03-13 12:37:15 +0800

    feat: add user roles and update constants; remove obsolete Client model

--- constants/constants.js
+++ constants/constants.js
@@ -4,6 +4,10 @@ dotenv.config();
 const PORT = process.env.PORT;
 const API_VERSION = process.env.API_VERSION;
 const APP_NAME = `JAF Live Chat`;
+const DB_URI = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI_PROD : process.env.MONGO_URI_LOCAL;
+
+const MASTER_DB_NAME = 'jafchatra_master';
+const TENANT_DB_PREFIX = 'tenant_';
 
 const CORS_OPTIONS = {
   origin: '*',
@@ -26,12 +30,31 @@ const SUBSCRIPTION_PLANS = {
   FREE_INTERNAL: 'FREE_INTERNAL',
 };
 
+const USER_STATUS = {
+  AVAILABLE: 'AVAILABLE',
+  BUSY: 'BUSY',
+  OFFLINE: 'OFFLINE',
+  AWAY: 'AWAY',
+};
 
-const MASTER_DB_NAME = 'jafchatra_master';
-const TENANT_DB_PREFIX = 'tenant_';
+const USER_ROLES = {
+  ADMIN: {
+    label: 'Admin',
+    value: 'ADMIN',
+  },
+  SUPPORT_AGENT: {
+    label: 'Support Agent',
+    value: 'SUPPORT_AGENT',
+  },
+  CUSTOMER: {
+    label: 'Customer',
+    value: 'CUSTOMER',
+  }
+}
 
 export {
   PORT,
+  DB_URI,
   API_VERSION,
   APP_NAME,
   CORS_OPTIONS,
@@ -39,4 +62,6 @@ export {
   SUBSCRIPTION_PLANS,
   MASTER_DB_NAME,
   TENANT_DB_PREFIX,
+  USER_STATUS,
+  USER_ROLES,
 };
