11300f4fcf4f976020df566615aff9dec7883644
darrylemiles <darryle.jafdigitalgroupinc@gmail.com>
2026-03-25 16:00:14 +0800

    feat: remove unused subscription and payment model imports from tenantServices.js

--- services/master/tenantServices.js
+++ services/master/tenantServices.js
@@ -1,7 +1,5 @@
 import { getTenantModel } from '../../models/master/Tenants.js';
 import { getMasterConnection } from '../../config/masterDB.js';
-import { getSubscriptionModel } from '../../models/master/Subscriptions.js';
-import { getPaymentModel } from '../../models/master/Payments.js';
 import { InternalServerError } from '../../utils/errors.js';
 import { logger } from '../../utils/logger.js';
 
@@ -33,8 +31,6 @@ const createTenant = async (tenantData, options = {}) => {
 const getTenantsByQuery = async (query) => {
   const { connection } = getMasterConnection();
   const Tenant = getTenantModel(connection);
-  const Subscription = getSubscriptionModel(connection);
-  const Payment = getPaymentModel(connection);
 
   try {
     const tenants = await Tenant.find(query);
